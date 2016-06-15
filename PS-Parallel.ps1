ef5eacd1622615296c809f0d791273958a642c69
Ryan Parker-Hill <ryan.parker-hill@deakin.edu.au>
2016-06-15 16:39:06 +1000

    Added -RepeatForever Added -ReloadCommandFile Fixed input. Now accepts commands as pipelined array, parameter array or specified text file.

--- PS-Parallel.ps1
+++ PS-Parallel.ps1
@@ -3,64 +3,108 @@
     PS-Parallel.ps1
 .DESCRIPTION 
     Run multiple commands in parallel.
+    Commands are accepted as a piplined array, or as a text file with one command per line.
     There is overhead when stating powershell jobs, this command is only beneficial when the commands you specify are long running.
 .EXAMPLE
-    Get-Content command_list.txt | PS-Parallel.ps1
+
+    # Execute commands in cmds.txt in parallel
+    Get-Content cmds.txt | PS-Parallel.ps1
+    PS-Parallel -Commands cmds.txt
+
+    # Exectue commands in cmds.txt forever without running duplicate commands
+    PS-Parallel -Commands cmds.txt -RepeatForever
+
+    # Execute commands in cmds.txt forever and reload the command list
+    PS-Parallel -Commands cmds.txt -ReloadCommmandFile -RepeatForever
+
 .NOTES 
-    Authors    : Ryan Parker-Hill <ryanph@aspersion.net>, Andrew Van Slageren
+    Authors    : Ryan Parker-Hill <ryanph@aspersion.net>
 .LINK 
     https://github.com/ryanph/PS-Parallel
 #> 
 
 Param
 (
-    [Parameter(Mandatory=$true,ValueFromPipeline=$true)]    [string[]]$Commands,
-    [Parameter(Mandatory=$false)]              [int] $MaxConcurrent = 3,
-    [Parameter(Mandatory=$false)]              [int] $JobPollInterval = 5
+    [Parameter(Mandatory=$true,ValueFromPipeline=$true)] [String[]]$Commands,
+    [Parameter(Mandatory=$false)] [int] $MaxConcurrent = 3,
+    [Parameter(Mandatory=$false)] [int] $JobPollInterval = 5,
+    [Parameter(Mandatory=$false)] [switch] $RepeatForever = $false,
+    [Parameter(Mandatory=$false)] [switch] $ReloadCommmandFile = $false
 )
 
 Set-StrictMode -Version 3
-$CommandList = [System.Collections.ArrayList]$Commands
-if ( $input -is [array] ) {
-    $CommandList = [System.Collections.ArrayList]$input
-}
-if ( ! $CommandList ) {
-    Throw "Command list is empty"
-}
 filter timestamp {"$(Get-Date -Format s) $_"}
+
 $JobPrefix = "PS-Parallel-"+$pid
 $JobNameMap = @{}
 
-Write-Output "Executing the $($CommandList.Count) specified commands $MaxConcurrent at a time" | timestamp
+Function Get-CommandList {
+    if ( $input -is [array] -and $input.Count -gt 1 ) {
+        if ( $ReloadCommmandFile ) {
+            Throw "Cannot reload command file when input is pipelined"
+        }
+        return $input.Clone()
+    } elseif ( $Commands -is [array] -and $Commands.Count -gt 1 ) {
+        if ( $ReloadCommmandFile ) {
+            Throw "Cannot reload command file when input is specified as an array parameter"
+        }
+        return $Commands.Clone()
+    } elseif ( $Commands -is [array] -and $Commands.Count -eq 1 ) {
+        return $(Get-Content $Commands[0])
+    } else {
+        Throw "No command list or file specified"
+    }
+}
 
-While ( $CommandList.Count -gt 0 -or $(get-job | ? {$_.Name -like "$($JobPrefix)_*"} | Measure-Object).count -gt 0 ) {
+$CommandQueue = [System.Collections.ArrayList] $(Get-CommandList)
+Write-Output "Executing the $($CommandQueue.Count) specified commands $MaxConcurrent at a time" | timestamp
+$JobNumber = 0
 
+# Loop while jobs are present or commands are in the list
+While ( $CommandQueue.Count -gt 0 -or $(get-job | ? {$_.Name -like "$($JobPrefix)_*"} | Measure-Object).count -gt 0 ) {
+
+    # Handle completed jobs
     get-job | ? {$_.Name -like "$($JobPrefix)*"} | Foreach-Object {
-	    if( $_.State -eq "Running" ) {
-		    return
-	    }
+        if( $_.State -eq "Running" ) {
+            return
+        }
         $JobCommand = $JobNameMap.Get_Item($_.Name)
-        Write-Output "Job $($_.Name) ($JobCommand) completed at $($_.PSEndTime) (Duration $($_.PSEndTime - $_.PSBeginTime))" | timestamp
-        Receive-Job $_.Id -ErrorAction "SilentlyContinue" 
-		Remove-Job $_.Id -WhatIf:$false
+        Write-Output "Job Complete $($_.Name): $JobCommand" | timestamp
+        Write-Output "  Finished at $($_.PSEndTime) after $($_.PSEndTime - $_.PSBeginTime)" | timestamp
+        Receive-Job $_.Id -ErrorAction "SilentlyContinue"
+        Remove-Job $_.Id -WhatIf:$false
     }
 
-    
-    while ( $(get-job -state Running | ? {$_.Name -like "$($JobPrefix)_*"} | Measure-Object).count -lt $MaxConcurrent -and $CommandList.Count -gt 0 ) {
+    # Fire off new jobs
+    while ( $(get-job -state Running | ? {$_.Name -like "$($JobPrefix)_*"} | Measure-Object).count -lt $MaxConcurrent -and $CommandQueue.Count -gt 0 ) {
         
-        $Command = $CommandList[0]
-        $CommandList.Remove($Command)
+        $Command = $CommandQueue[0]
+        $CommandQueue.Remove($Command)
 
         if ( $Command ) {
-            $JobName = "$($JobPrefix)_$($CommandList.Count)"
+            $JobNumber = $JobNumber + 1
+            $JobName = "$($JobPrefix)_$($JobNumber)"
             $JobNameMap.Set_Item($JobName, $Command)
-            Write-Output "Starting job $JobName with command $Command" | timestamp
+            Write-Output "Job Start $($JobName): $Command" | timestamp
             $job = Start-Job -Name $JobName -ScriptBlock { Invoke-Expression $args[0] } -ArgumentList $Command
+        }
+    }
+
+    # Repopulate the CommandQueue
+    if ( $CommandQueue.Count -eq 0 -and $RepeatForever -and $(get-job -state Running | ? {$_.Name -like "$($JobPrefix)_*"} | Measure-Object).count -lt $MaxConcurrent) {
+
+        Write-Output "All queued jobs have executed. Populating command queue." | timestamp
+        $CommandQueue = [System.Collections.ArrayList] $(Get-CommandList)
+
+        foreach ( $RunningCommand in $(get-job -state Running | Select -Property Name) ) {
+            if ( $CommandQueue.Contains($JobNameMap[$RunningCommand.Name]) ) {
+                Write-Output "  Not re-queueing running command: $($JobNameMap[$RunningCommand.Name])" | timestamp
+                $CommandQueue.Remove($JobNameMap[$RunningCommand.Name])
+            }
         }
     }
 
     Start-Sleep -s $JobPollInterval
 
 }
-
 
--- README.md
+++ README.md
@@ -2,13 +2,24 @@
 
 Run a list of commands in parallel (like GNU Parallel).
 
+Commands are accepted as a piplined array, or as a text file with one command per line.
+
 The overhead in starting Powershell jobs for each command makes this only really benficial when you have a lot of long running commands you want to get through.
 
-By default execute 3 commands at a time.
+By default executes 3 commands at a time.
 
 ## Examples
-- Get-Content command_list.txt | PS-Parallel.ps1
-- Get-Content command_list.txt | PS-Parallel.ps1 -MaxConcurrent 10
+### Execute commands in cmds.txt in parallel
+- Get-Content cmds.txt | PS-Parallel.ps1
+- Get-Content cmds.txt | PS-Parallel.ps1 -MaxConcurrent 10
+- PS-Parallel -Commands cmds.txt
+
+### Exectue commands in cmds.txt repeatedly (without running twice at the same time)
+- PS-Parallel -Commands cmds.txt -RepeatForever
+- Get-Content cmds.txt | PS-Parallel.ps1 -RepeatForever
+
+### Same as above but check the command file for new commands as well
+- PS-Parallel -Commands cmds.txt -ReloadCommmandFile -RepeatForever
 
 ## Contribute
 Please, my powershell is terrible.
